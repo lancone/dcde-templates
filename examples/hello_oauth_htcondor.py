128633cb44e4380f359c5dbe20f670f00ff0e373
jhover <jhover@starkhorn.rhic.bnl.gov>
2019-07-30 10:39:48 -0400

    Update.

--- examples/hello_oauth_htcondor.py
+++ examples/hello_oauth_htcondor.py
@@ -12,33 +12,32 @@ from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
 from parsl.app.app import python_app, bash_app
 
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+parsl.set_stream_logger()
 
 config = Config(
     executors=[
-        HighThroughputExecutor(
-            label='condor_oauth_ssh',
-            provider=CondorProvider(
-                channel=OAuthSSHChannel(
-                    hostname='spce01.sdcc.bnl.gov',
-                    port=2222,
-                    username='dcde1000001',     # Please replace USERNAME with your username
-                    script_dir='/sdcc/u/dcde1000001/parsl_scripts',    # Please replace USERNAME with your username
-                ),
-                #nodes_per_block=1,
-                #init_blocks=4,
-                #max_blocks=4,
-                scheduler_options='accounting_group = group_sdcc.main',
-                #worker_init='',     # Input your worker_init if needed,
-               
-            ),
-            #address='130.199.185.9' # The address of the login host
-            #controller=Controller(public_ip='130.199.16.40'),    # Please replace PUBLIC_IP with your public ip
-        )
+            HighThroughputExecutor(
+                    address='128.135.123.206',
+                    label='condor_oauth_ssh',
+                    worker_debug=True,
+                    worker_logdir_root='/sdcc/u/dcde1000001/parsl_scripts/logs',
+                    working_dir='/sdcc/u/dcde1000001/parsl_scripts',
+                    provider=CondorProvider(
+                            channel=OAuthSSHChannel(
+                                    hostname='spce01.sdcc.bnl.gov',
+                                    port=2222,
+                                    username='dcde1000001',     # Please replace USERNAME with your username
+                                    script_dir='/sdcc/u/dcde1000001/parsl_scripts',    # Please replace USERNAME with your username
+                            ),
+                            #nodes_per_block=1,
+                            init_blocks=1,
+                            #max_blocks=4,
+                            scheduler_options='accounting_group = group_sdcc.main',
+                            worker_init='source ~/setup.sh',     # Input your worker_init if needed,
+                    ),
+            )
     ],
 )
-
 
 parsl.clear()
 parsl.load(config)
