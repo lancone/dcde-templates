e103fb0b2cfa96fe4af44fab3a3e6bce63c12062
jhover <jhover@starkhorn.rhic.bnl.gov>
2019-08-21 18:31:29 -0400

    Update.

--- examples/bnl-config-test.py
+++ examples/bnl-config-test.py
@@ -19,30 +19,59 @@ def sdcc_wninfo():
     import os
     return os.uname()
 
-logging.basicConfig(format='%(asctime)s (UTC) [%(levelname)s] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s')
-logging.getLogger().setLevel(logging.DEBUG)
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s (UTC) [%(levelname)s] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s')
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--conffile', 
+                        action="store", 
+                        dest='conffile', 
+                        default="/etc/dcde/dcdeparsl.conf",
+                        help='configuration file.')
 
-#parsl.set_stream_logger(level=0)
-parsl.clear()
+    parser.add_argument('-u', '--username', 
+                        action="store", 
+                        dest='username', 
+                        default="dcde1000001",
+                        help='remote username')
 
-logging.info("getting config for spce01, dcde1000001")
-cf = ConfigFactory()
-bnl_sdcc_condor = cf.getconfig( 'spce01.sdcc.bnl.gov', 'dcde1000001')
+    parser.add_argument('-e', '--endpoint', 
+                        action="store", 
+                        dest='endpoint', 
+                        default="spce01.sdcc.bnl.gov",
+                        help='remote submit host')
+    
+    parser.add_argument('-d', '--debug', 
+                        action="store_true", 
+                        dest='debug', 
+                        help='debug logging')
+    args = parser.parse_args()
 
-logging.info("loading config: %s" % bnl_sdcc_condor)
-
-parsl.load(bnl_sdcc_condor)
-
-#condorinfo = sdcc_wninfo(executors=['sdcc_condor'])
-logging.info("submitting...")
-condorinfo = sdcc_wninfo(stdout='relion_condor.out', stderr='relion_condor.err')
-
-logging.info("awaiting result....")
-
-# Must. wait. for. job. to. finish.
-condorinfo.result()
-
-logging.info("got result, printing...")
-
-with open (condorinfo.stdout, 'r') as f:
-    print(condorinfo.read())
+    logging.basicConfig(format='%(asctime)s (UTC) [%(levelname)s] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s')
+    logging.getLogger().setLevel(logging.DEBUG)
+    
+    #parsl.set_stream_logger(level=0)
+    parsl.clear()
+    
+    logging.info("getting configfactory with conffile: %s" %  args.conffile)
+    cf = ConfigFactory(conffile=args.conffile)
+    logging.info("getting config for spce01.sdcc.bnl.gov, dcde1000001")
+    bnl_sdcc_condor = cf.getconfig( args.endpoint, args.conffile)
+    
+    logging.info("loading config: %s" % bnl_sdcc_condor)
+    
+    parsl.load(bnl_sdcc_condor)
+    
+    #condorinfo = sdcc_wninfo(executors=['sdcc_condor'])
+    logging.info("submitting...")
+    condorinfo = sdcc_wninfo(stdout='relion_condor.out', stderr='relion_condor.err')
+    
+    logging.info("awaiting result....")
+    
+    # Must. wait. for. job. to. finish.
+    condorinfo.result()
+    
+    logging.info("got result, printing...")
+    
+    with open (condorinfo.stdout, 'r') as f:
+        print(condorinfo.read())
